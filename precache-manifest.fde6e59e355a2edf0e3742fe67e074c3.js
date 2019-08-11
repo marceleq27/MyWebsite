8cb79b5d9416852f44b8259d66440ce35fdcd664
Marcel Bednarz <marcel.bednarz16@gmail.com>
2019-08-12 01:28:08 +0200

    Updates

---
+++ precache-manifest.fde6e59e355a2edf0e3742fe67e074c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ae1fa67a35b85a32f8da953084d6177",
+    "url": "/MyWebsite/index.html"
+  },
+  {
+    "revision": "256d82d37dbbca167d14",
+    "url": "/MyWebsite/static/css/main.e96eac82.chunk.css"
+  },
+  {
+    "revision": "753a310baad3e9cf7090",
+    "url": "/MyWebsite/static/js/2.87d557cd.chunk.js"
+  },
+  {
+    "revision": "256d82d37dbbca167d14",
+    "url": "/MyWebsite/static/js/main.7a7c8cbd.chunk.js"
+  },
+  {
+    "revision": "77be4a14ab2346968cf8",
+    "url": "/MyWebsite/static/js/runtime~main.92cfa0b6.js"
+  },
+  {
+    "revision": "b21693667bd67bef4b693d72e8082c23",
+    "url": "/MyWebsite/static/media/zdj2.b2169366.jpeg"
+  }
+]);
